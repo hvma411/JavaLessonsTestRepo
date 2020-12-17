4ae85c36a1b989d30de54264660e63a741c6ae7a
Nosek Kamil <Kamil.Nosek@t-mobile.pl>
2020-12-17 10:14:18 +0100

    first commit to new test java lessons repo

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String name = "Ronette";
+	    String lastName = "Pulaski";
+
+        System.out.printf("%s's surname is %s", name, lastName);
+    }
+}
